714847f0ae8c2c5fbcf2877387729af780fec98e
guilherme.borniotto <gsborniotto@gmail.com>
2026-03-24 09:43:12 -0300

    task: creation of the notification functionality

---
+++ src/OnboardingSIGDB1.Domain/Notifications/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System.Dynamic;
+
+namespace OnboardingSIGDB1.Domain.Notifications;
+
+public class Notification
+{
+    
+    public string Campo  { get; set; }
+    public string Descricao { get; set; }
+
+    public Notification(){}
+    public Notification(string campo, string descricao)
+    {
+       Campo = campo;
+       Descricao = descricao;
+    }
+}
